c33f57c97852e1a5598e406c637247a1190d50f1
Firmino Neto <firminoneto11@hotmail.com>
2022-09-07 19:34:03 -0300

    Fix: Removed debug level in log call

--- backend/src/populator.py
+++ backend/src/populator.py
@@ -21,7 +21,7 @@ async def main() -> NoReturn:
     # Loop that will be running forever to keep the database up to date with blizzard's data
     while True:
         # await fetch_blizzard_api(logger=logger)
-        await logger.log(f"Awaiting {UPDATE_EVERY} seconds before the next requests round", level="debug")
+        await logger.log(f"Awaiting {UPDATE_EVERY} seconds before the next requests round")
         await sleep(UPDATE_EVERY)
 
 
