3923c1425854c2e604c9e71d8fe373805f171b55
Firmino Neto <firminoneto11@hotmail.com>
2022-10-21 12:19:26 -0300

    Chore: Changed fields to be used by dict method

--- backend/src/apps/brackets/models/sessions.py
+++ backend/src/apps/brackets/models/sessions.py
@@ -8,3 +8,6 @@ class Sessions(BaseModel):
         tablename = "sessions"
 
     session: int = models.Integer(unique=True)
+
+    def dict(self) -> dict:
+        return super().dict(include={"session"})
--- backend/src/apps/brackets/models/wow_specs.py
+++ backend/src/apps/brackets/models/wow_specs.py
@@ -10,3 +10,6 @@ class WowSpecs(BaseModel):
     blizzard_id: int = models.BigInteger()
     name: str = models.String(max_length=50)
     icon_url: str = models.Text()
+
+    def dict(self) -> dict:
+        return super().dict(include={"blizzard_id", "name", "icon_url"})
