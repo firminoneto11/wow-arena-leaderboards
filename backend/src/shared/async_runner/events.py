11efd2c68ffa401e6ba282dfe36d29ffe7736266
Firmino Neto <firminoneto11@hotmail.com>
2022-10-23 12:07:09 -0300

    Chore: Created enum for event types

--- backend/src/shared/async_runner/events.py
+++ backend/src/shared/async_runner/events.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 from asyncio import Queue
+from enum import Enum
+
+
+class EventTypes(Enum):
+    CLI_SHUTDOWN = "cli-shutdown"
 
 
 @dataclass
@@ -12,7 +17,7 @@ class EventsQueue:
     _instance: "EventsQueue"
     _queue: Queue[Event]
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs) -> "EventsQueue":
         if not hasattr(cls, "_instance"):
             cls._instance = super().__new__(cls, *args, **kwargs)
             cls._instance._queue = Queue()
@@ -28,6 +33,6 @@ def get_events_queue() -> Queue[Event]:
 
 
 def emit_event(name: str) -> None:
-    event = Event(name=name)
+    event = Event(name=EventTypes[name].value)
     queue = get_events_queue()
     queue.put_nowait(item=event)
