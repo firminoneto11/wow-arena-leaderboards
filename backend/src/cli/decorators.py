c750062b2c3f18445d7e7f172322e657f9752eca
Firmino Neto <firminoneto11@hotmail.com>
2022-10-23 12:08:19 -0300

    Chore: Added more robust types to decorator

--- backend/src/cli/decorators.py
+++ backend/src/cli/decorators.py
@@ -1,19 +1,22 @@
-from typing import Callable, TypeVar as Generic
-from typing import Coroutine
+from typing import TypeVar, Awaitable, ParamSpec, Callable
 from functools import wraps
 
-from shared import emit_event
+from shared import emit_event, EventTypes
 
 
-_Coroutine = Generic("_Coroutine", bound=Coroutine)
-_Callable = Generic("_Callable", bound=Callable)
+_ParamTypes = ParamSpec("_ParamTypes")
+_AwaitableReturnType = TypeVar("_AwaitableReturnType")
 
 
-def close_event_loop_after_execution(coroutine: type[_Callable]) -> type[_Coroutine]:
-    @wraps(coroutine)
-    async def inner(*args, **kwargs):
-        coroutine_return = await coroutine(*args, **kwargs)
-        emit_event(name="cli-shutdown")
+def close_event_loop_after_execution(
+    callable_that_returns_an_awaitable: Callable[_ParamTypes, Awaitable[_AwaitableReturnType]]
+) -> Callable[_ParamTypes, Awaitable[_AwaitableReturnType]]:
+    """This decorator simply make sure that the main event loop is closed after the coroutine finishes it's execution."""
+
+    @wraps(callable_that_returns_an_awaitable)
+    async def decorator(*args: _ParamTypes.args, **kwargs: _ParamTypes.kwargs) -> _AwaitableReturnType:
+        coroutine_return = await callable_that_returns_an_awaitable(*args, **kwargs)
+        emit_event(name=EventTypes.CLI_SHUTDOWN.name)
         return coroutine_return
 
-    return inner
+    return decorator
