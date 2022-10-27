167e1682abfbc6d2fb478d23f3d46d6b2874203f
Firmino Neto <firminoneto11@hotmail.com>
2022-10-27 19:54:08 -0300

    Chore: Commented some code to prevent requests

--- backend/src/service/__init__.py
+++ backend/src/service/__init__.py
@@ -21,23 +21,23 @@ async def fetch_blizzard_api(logger: Logger) -> None:
     latest_session_id, latest_session = await get_latest_session()
 
     # Fetching the access token in order to make the other requests
-    response = await fetch_token(logger=logger)
+    # response = await fetch_token(logger=logger)
 
     # Fetching pvp data, wow classes's data and wow specs's data concurrently
-    pvp_data, wow_classes, wow_specs = await gather(
-        fetch_pvp_data(logger=logger, access_token=response.access_token, latest_session=latest_session),
-        fetch_wow_classes(logger=logger, access_token=response.access_token),
-        fetch_wow_specs(logger=logger, access_token=response.access_token),
-    )
+    # pvp_data, wow_classes, wow_specs = await gather(
+    #     fetch_pvp_data(logger=logger, access_token=response.access_token, latest_session=latest_session),
+    #     fetch_wow_classes(logger=logger, access_token=response.access_token),
+    #     fetch_wow_specs(logger=logger, access_token=response.access_token),
+    # )
 
     # Waiting so we don't get throttled
-    create_task(logger.info(f"Awaiting {DELAY} seconds in order to not get throttled."))
-    await sleep(DELAY)
+    # create_task(logger.info(f"Awaiting {DELAY} seconds in order to not get throttled."))
+    # await sleep(DELAY)
 
     # Fetching the wow players's media
-    pvp_data = await fetch_wow_media(logger=logger, access_token=response.access_token, pvp_data=pvp_data)
+    # pvp_data = await fetch_wow_media(logger=logger, access_token=response.access_token, pvp_data=pvp_data)
 
-    await dump_data(pvp_data=pvp_data, wow_classes=wow_classes, wow_specs=wow_specs)
+    # await dump_data(pvp_data=pvp_data, wow_classes=wow_classes, wow_specs=wow_specs)
     pvp_data, wow_classes, wow_specs = await read_data()
 
     create_task(
