5590989fbfc93bc2a895833b30a8970b955e9504
Firmino Neto <firminoneto11@hotmail.com>
2022-12-11 12:21:59 -0300

    Chore: Added dynamic apps discovery

--- backend/api/config/settings.py
+++ backend/api/config/settings.py
@@ -3,35 +3,44 @@ from pathlib import Path as _Path
 
 from dynaconf import Dynaconf as _Dynaconf
 
+
 # Base directory of the project
 BASE_DIR: _Final = _Path(__file__).resolve().parent.parent.parent
+
 
 # Directory where the apps can be found
 APPS_DIR: _Final = BASE_DIR / "api" / "apps"
 
-# Directory where the logs can be stored
-LOGS_DIR: _Final = BASE_DIR / ".logs"
-
-# Prefix for environment variables
-ENV_PREFIX: _Final = "ARENA_API"
-
-# Secrets files
-SETTINGS_FILES: _Final = ["env.toml"]
 
 # Apps installed
 INSTALLED_APPS: _Final = ["core"]
 
+
+# Directory where the logs can be stored
+LOGS_DIR: _Final = BASE_DIR / ".logs"
+
+
+# Prefix for environment variables
+ENV_PREFIX: _Final = "ARENA_API"
+
+
+# Secrets files
+SETTINGS_FILES: _Final = ["env.toml"]
+
+
 # Env object
 ENV: _Final = _Dynaconf(envvar_prefix=ENV_PREFIX, settings_files=SETTINGS_FILES)
 
+
 # DB connection string
 DATABASE_URL: _Final[str] = ENV.database_url
+
 
 TORTOISE_ORM = {
     "connections": {"default": DATABASE_URL},
     "apps": {
         "models": {
-            "models": ["api.apps.core.models"],
+            "models": [APPS_DIR / app / "models" for app in INSTALLED_APPS],
             "default_connection": "default",
         },
     },
