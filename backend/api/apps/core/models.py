b1f80a75c5440caf4ec4da67f7128bae0072b428
Firmino Neto <firminoneto11@hotmail.com>
2022-12-12 21:08:38 -0300

    Chore: Moved models to a single file

--- backend/api/apps/core/models.py
+++ backend/api/apps/core/models.py
@@ -1,9 +1,34 @@
 from tortoise import fields
 
 from shared.models import BaseModel
-from .wow_classes import WowClasses
-from .wow_specs import WowSpecs
-from .sessions import Sessions
+
+
+class Sessions(BaseModel):
+    class Meta:
+        table = "sessions"
+        ordering = ["-session"]
+
+    session: int = fields.IntField()
+
+
+class WowClasses(BaseModel):
+    class Meta:
+        table = "wow_classes"
+        ordering = ["-blizzard_id"]
+
+    blizzard_id: int = fields.BigIntField()
+    name: str = fields.CharField(max_length=100)
+    icon_url: str | None = fields.TextField(null=True, default=None)
+
+
+class WowSpecs(BaseModel):
+    class Meta:
+        table = "wow_specs"
+        ordering = ["-blizzard_id"]
+
+    blizzard_id: int = fields.BigIntField()
+    name: str = fields.CharField(max_length=100)
+    icon_url: str | None = fields.TextField(null=True, default=None)
 
 
 class PvpData(BaseModel):
@@ -25,7 +50,7 @@ class PvpData(BaseModel):
 
     # FKs - Required
     session: Sessions = fields.ForeignKeyField(
-        model_name="api.apps.core.models.Sessions", related_name="ladder_data", on_delete=fields.CASCADE
+        model_name="core.Sessions", related_name="ladder_data", on_delete=fields.CASCADE
     )
 
     # Optional Fields
@@ -33,15 +58,15 @@ class PvpData(BaseModel):
 
     # Fks - Optionals
     wow_class: WowClasses | None = fields.ForeignKeyField(
-        model_name="api.apps.core.models.WowClasses",
-        nullable=True,
+        model_name="core.WowClasses",
+        null=True,
         default=None,
         on_delete=fields.SET_NULL,
         related_name="ladder_data",
     )
     wow_spec: WowSpecs | None = fields.ForeignKeyField(
-        model_name="api.apps.core.models.WowSpecs",
-        nullable=True,
+        model_name="core.WowSpecs",
+        null=True,
         default=None,
         on_delete=fields.SET_NULL,
         related_name="ladder_data",
--- backend/api/apps/core/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .sessions import Sessions
-
-from .wow_classes import WowClasses
-from .wow_specs import WowSpecs
-
-from .pvp_data import PvpData
