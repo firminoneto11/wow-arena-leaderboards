2cba68e905ca6cd799d4d13644aefdcc960d289f
Firmino Neto <firminoneto11@hotmail.com>
2022-12-14 17:26:55 -0300

    Added settings for tortoise_orm

--- backend/api/conf/settings.py
+++ backend/api/conf/settings.py
@@ -9,7 +9,11 @@ BASE_DIR: _Final = _Path(__file__).resolve().parent.parent.parent
 
 
 # Directory where the apps can be found
-APPS_DIR: _Final = BASE_DIR / "api" / "apps"
+# APPS_DIR: _Final = BASE_DIR / "api" / "apps"
+
+
+# Apps module
+APPS_MODULE = "api.apps"
 
 
 # Apps installed
@@ -39,9 +43,15 @@ DATABASE_URL: _Final[str] = ENV.database_url
 TORTOISE_ORM = {
     "connections": {"default": DATABASE_URL},
     "apps": {
-        "models": {
-            "models": [APPS_DIR / app / "models" for app in INSTALLED_APPS],
+        app: {
+            "models": [f"{APPS_MODULE}.{app}.models"],
             "default_connection": "default",
-        },
+        }
+        for app in INSTALLED_APPS
     },
 }
+
+
+# The 'aerich.models' module has to be added only in the first one
+first_key = list(TORTOISE_ORM["apps"])[0]
+TORTOISE_ORM["apps"][first_key]["models"].append("aerich.models")
