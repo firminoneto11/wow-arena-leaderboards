70c9ed710ea43215774ef4ab791191249e4fb32e
Firmino Neto <firminoneto11@hotmail.com>
2022-12-31 20:25:50 -0300

    Chore: Added custom attrs for the exceptions

--- backend/service/exceptions.py
+++ backend/service/exceptions.py
@@ -7,8 +7,20 @@ class CouldNotExecuteError(Exception):
 
 
 class NetworkError(Exception):
-    ...
+    detail: str
+    formatted_traceback: str
+
+    def __init__(self, detail: str, traceback: str = "") -> None:
+        self.detail = detail
+        self.formatted_traceback = traceback
+        super().__init__(detail)
 
 
 class StatusCodeError(Exception):
-    ...
+    detail: str
+    extra_data: dict | None
+
+    def __init__(self, detail: str, extra_data: dict | None = None) -> None:
+        self.detail = detail
+        self.extra_data = extra_data
+        super().__init__(detail)
